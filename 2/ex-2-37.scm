74d31f52d4dbb545b9536ec79e1b19a65ca510b8
Kevin Boyer <kevinjboyer@gmail.com>
2020-05-12 08:12:24 -0400

    thru 2.39

--- 2/ex-2-37.scm
+++ 2/ex-2-37.scm
@@ -1,5 +1,5 @@
 #lang sicp
-; todo
+
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
@@ -18,8 +18,35 @@
              init
              (accumulate (lambda (x y) (cons (cdr x) y)) nil seqs)))))
 
-(define s (list (list 1 2 3)
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map
+   (lambda (w) (dot-product v w))
+   m))
+
+(define (transpose m)
+  (accumulate-n cons nil m))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map
+     (lambda (v) (map (lambda (w) (dot-product v w)) cols))
+     m)))
+
+(define m (list (list 1 2 3)
                 (list 4 5 6)
                 (list 7 8 9)
                 (list 10 11 12)))
-(accumulate-n + 0 s)
+(define n (list (list 1 2 3 4)
+                (list 5 6 7 8)
+                (list 9 10 11 12)))
+    
+(define v (list 1 2 3))
+(define w (list 4 5 6))
+
+;(dot-product v w)
+;(matrix-*-vector m v)
+;(transpose n)
+(matrix-*-matrix m n)
