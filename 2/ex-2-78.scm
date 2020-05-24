540cf2f8998ba1ae4590aca493c15ea2397e50cf
Kevin Boyer <kevinjboyer@gmail.com>
2020-05-24 09:11:51 -0400

    through 2.81

---
+++ 2/ex-2-78.scm
@@ -0,0 +1,16 @@
+#lang sicp
+
+(define (type-tag datum)
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (attach-tag type-tag contents)
+  (if (eq? type-tag 'scheme-number)
+      contents
+      (cons type-tag contents)))
+
+(define (contents datum)
+  (cond ((number? datum) datum)
+        ((pair? datum) (car datum))
+        (error "Bad tagged datum: CONTENTS" datum)))
