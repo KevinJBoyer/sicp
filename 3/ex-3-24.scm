4547c15e6ed3cc3f821113ac35a0760b823b12d5
Kevin Boyer <kevinboyer@navapbc.com>
2024-06-17 20:03:53 -0400

    thru 3.24

---
+++ 3/ex-3-24.scm
@@ -0,0 +1,52 @@
+#lang sicp
+
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record (cdr record) false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define (within-one? x y)
+  (if (< (abs (- x y)) 1)
+      true
+      false
+      ))
+
+(define operation-table (make-table within-one?))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+; testing
+(put 3 5 'hello)
+(get 3.9 4.1)
+(get 4 5)
